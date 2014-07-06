940882e8591d3eded947b454cebccb00e47b5123
blackcrack <blackcrack@blackysgate.de>
2014-07-06 13:07:52 +0200

    Update upb.initialize.php
    Change:
    
    $errorHandler = &amp;amp;new errorhandler();
    to
    $errorHandler = new errorhandler();
    
    http://www.basildon.com/upb/viewtopic.php?id=5&t_id=22

--- includes/upb.initialize.php
+++ includes/upb.initialize.php
@@ -128,7 +128,7 @@ if(function_exists("date_default_timezone_set")) {
 
 require_once("./includes/inc/defines.inc.php");
 require_once("./includes/class/error.class.php");
-$errorHandler = &new errorhandler();
+$errorHandler = new errorhandler();
 //set_error_handler(array(&$errorHandler, 'add_error'));
 error_reporting(E_ALL ^ E_NOTICE);
 
